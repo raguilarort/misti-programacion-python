5dcc0e3d11c64527b41e798606a20914066abb34
RICARDO AGUILAR ORTEGA <ricardo.aguilar@senado.gob.mx>
2025-04-26 19:35:07 -0600

    P3.EstructuraDatos.Feat. Se actualiza uso de los nodos en la pila para su operación

--- Practicas/03_Practica3_Estructuras_Datos/Modelo/Estructuras/Pila/Pila.py
+++ Practicas/03_Practica3_Estructuras_Datos/Modelo/Estructuras/Pila/Pila.py
@@ -1,11 +1,4 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[5]:
-class Nodo:#clase que representará un nodo en la pila. cada nodo contendrá un valor y un puntero al siguiente nodo en la pila
-    def __init__(self, valor):
-        self.valor = valor  #asigna el valor recibido al atributo valor del nodo. Este valor es lo que almacenará el nodo
-        self.siguiente = None  #inicializa el atributo siguiente como None. este atributo actuará como un puntero al siguiente nodo en la pila. al principio, no hay otro nodo, por lo que se establece como None
+from Modelo.Nodos.Nodo import Nodo
 
 class Pila:#esta clase manejará la estructura de la pila utilizando nodos
     def __init__(self):#constructor de la clase
@@ -13,7 +6,7 @@ class Pila:#esta clase manejará la estructura de la pila utilizando nodos
     
     def insertar(self, numero):#metodo para insertar elementos push. recibe un numero como argumento
         nuevo_nodo = Nodo(numero)  #crea un nuevo nodo con el numero ingresado
-        nuevo_nodo.siguiente = self.tope  #el siguiente nodo del nuevo nodo será el nodo que estaba en el tope
+        nuevo_nodo.right = self.tope  #el siguiente nodo del nuevo nodo será el nodo que estaba en el tope
         self.tope = nuevo_nodo  #actualiza el atributo tope de la pila para que apunte al nuevo nodo
     
     def mostrar(self):
@@ -23,20 +16,19 @@ class Pila:#esta clase manejará la estructura de la pila utilizando nodos
             nodo_actual = self.tope
             print("Pila actual:", end=" ")#end=" " para asegurar que los valores se impriman en una sola línea
             while nodo_actual:#bucle que recorrera toda la pila
-                print(nodo_actual.valor, end=" ")
-                nodo_actual = nodo_actual.siguiente
+                print(nodo_actual.value, end=" ")
+                nodo_actual = nodo_actual.right
             print() 
     
     def pop(self):
         if self.tope:#verifica si la pila no esta vacía
-            valor_eliminado = self.tope.valor  #guardamos el valor del nodo a eliminar
-            self.tope = self.tope.siguiente  #actualiza el atributo tope para que apunte al siguiente nodo, eliminando efectivamente el nodo actual del tope de la pila
+            valor_eliminado = self.tope.value  #guardamos el valor del nodo a eliminar
+            self.tope = self.tope.right  #actualiza el atributo tope para que apunte al siguiente nodo, eliminando efectivamente el nodo actual del tope de la pila
             return valor_eliminado
         else:
             print("La pila está vacía.")
             return None
 
-# In[ ]:
 
 
 
