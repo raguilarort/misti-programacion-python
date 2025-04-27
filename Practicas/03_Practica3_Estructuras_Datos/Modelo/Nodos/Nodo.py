844e87253c9091f8285ac8d8effb44d03c8468c2
RICARDO AGUILAR ORTEGA <ricardo.aguilar@senado.gob.mx>
2025-04-26 20:20:25 -0600

    P3.EstructuraDatos.Style. Se agregan comentarios a la clase Nodo

--- Practicas/03_Practica3_Estructuras_Datos/Modelo/Nodos/Nodo.py
+++ Practicas/03_Practica3_Estructuras_Datos/Modelo/Nodos/Nodo.py
@@ -3,15 +3,18 @@ Clase que representa un nodo simple
 '''
 class Nodo:
     def __init__(self, v):
-        self.value = v
-        self.right = None
+        self.value = v #Atributo de instancia que almacena el valor del nodo
+        self.right = None #Atributo de instancia que almacena la referencia al nodo de la derecha o siguiente
 
+    #Método que muestra la información que contiene el nodo
     def mostrar_informacion(self):
         print(self.__str__)
 
+    #Método que permite insertar el valor que almacenará el nodo
     def guardar_informacion(self, v):
         self.valor = v
 
+    #Método que permite guardar la referencia al nodo de la derecha o siguiente
     def nuevo_siguiente(self, Nodo):
         self.siguiente = Nodo
 
