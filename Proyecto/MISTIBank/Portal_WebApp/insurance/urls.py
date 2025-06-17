e2d6c82e58a089448df9166cd8381b3a6481634d
Luz-Ojeda <luzojeda080@gmail.com>
2025-06-17 04:54:39 -0600

    creacion de tabla en bd para info de la grafica. modificacion de archivos para visualizar el boton para ver grafica

--- Proyecto/MISTIBank/Portal_WebApp/insurance/urls.py
+++ Proyecto/MISTIBank/Portal_WebApp/insurance/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('home/', views.hello),
     path('about/', views.about),
     path('login/', views.login),
-    path('dashboard/', views.dashboard)
+    path('dashboard/', views.dashboard),
+    path('grafica-usuario/', views.grafica_seguros_usuario, name='grafica_usuario'),
 ]
--- Proyecto/MISTIBank/Portal_WebApp/insurance/views.py
+++ Proyecto/MISTIBank/Portal_WebApp/insurance/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import SeguroAuto
+from django.contrib.auth.decorators import login_required
+from django.utils.timezone import now, timedelta
+from collections import defaultdict
 
 # Create your views here.
 def hello(request):
@@ -16,4 +20,23 @@ def about(request):
     return HttpResponse("About")
 
 
+@login_required
+def grafica_seguros_usuario(request):
+    hoy = now().date()
+    hace_7_dias = hoy - timedelta(days=6)
 
+    seguros = SeguroAuto.objects.filter(usuario=request.user, fecha_contratacion__range=[hace_7_dias, hoy])
+
+    datos = defaultdict(int)
+    for s in seguros:
+        datos[s.fecha_contratacion.strftime("%Y-%m-%d")] += 1
+
+    # Completar días sin registros
+    dias = [(hace_7_dias + timedelta(days=i)).strftime("%Y-%m-%d") for i in range(7)]
+    valores = [datos[dia] for dia in dias]
+
+    context = {
+        "labels": dias,
+        "valores": valores
+    }
+    return render(request, "grafica_usuario.html", context)
