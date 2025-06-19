5e97c33d5a31b91f91e10f96112360f63788c48d
Luz-Ojeda <luzojeda080@gmail.com>
2025-06-19 02:20:46 -0600

    Revert "creacion de tabla en bd para info de la grafica. modificacion de archivos para visualizar el boton para ver grafica"
    This reverts commit e2d6c82e58a089448df9166cd8381b3a6481634d.

--- Proyecto/MISTIBank/Portal_WebApp/insurance/models.py
+++ Proyecto/MISTIBank/Portal_WebApp/insurance/models.py
@@ -1,11 +1,3 @@
 from django.db import models
-from django.contrib.auth.models import User
 
-class SeguroAuto(models.Model):
-    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
-    tipo = models.CharField(max_length=100)
-    fecha_contratacion = models.DateField(auto_now_add=True)
-    costo = models.DecimalField(max_digits=10, decimal_places=2)
-
-    def __str__(self):
-        return f"{self.usuario.username} - {self.tipo} ({self.fecha_contratacion})"
+# Create your models here.
--- Proyecto/MISTIBank/Portal_WebApp/insurance/urls.py
+++ Proyecto/MISTIBank/Portal_WebApp/insurance/urls.py
@@ -5,6 +5,5 @@ urlpatterns = [
     path('home/', views.hello),
     path('about/', views.about),
     path('login/', views.login),
-    path('dashboard/', views.dashboard),
-    path('grafica-usuario/', views.grafica_seguros_usuario, name='grafica_usuario'),
+    path('dashboard/', views.dashboard)
 ]
